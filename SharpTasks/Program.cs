f706eab670390c1be9a315ce4f9d4a06129c4150
Rah.heidari <rahmatheydari480@gmail.com>
2025-06-16 06:35:40 +0330

    Initial commit

---
+++ SharpTasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
